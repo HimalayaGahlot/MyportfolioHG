e669a0fa07943187de271c795c6067eea1923f76
himalaya-10 <98940477+himalaya-10@users.noreply.github.com>
2024-03-22 17:23:21 +0530

    progress1

--- pages/logo.js
+++ pages/logo.js
@@ -1,4 +1,7 @@
 export default function logo() {
+  const onClick=()=>{
+
+  }
     return (
       <div className="logo flex justify-center m-2 h-[50px] w-[40px]" >
         <style jsx>{`
@@ -10,7 +13,7 @@ export default function logo() {
                       height: 0;
                       border-left: 36px solid transparent; 
                       border-right: 19.33px solid transparent; 
-                      border-bottom: 24.333px solid  rgb(201,162,265); 
+                      border-bottom: 24.333px solid  rgb(233,234,236); 
                     }
                     .triangle2{
                       position:absolute;
@@ -20,7 +23,7 @@ export default function logo() {
                       height: 0;
                       border-left: 36.83px solid transparent; 
                       border-right: 34.66px solid transparent; 
-                      border-bottom: 10.17px solid  rgb(201,162,265); 
+                      border-bottom: 10.17px solid  rgb(233,234,236);
                     }
                     .triangle3{
                       position:absolute;
@@ -30,13 +33,14 @@ export default function logo() {
                       height: 0;
                       border-left: 17.66px solid transparent; 
                       border-right: 41.16px solid transparent; 
-                      border-bottom: 17px solid rgb(201,162,265);
+                      border-bottom: 17px solid rgb(233,234,236); 
                     }
                     `}
         </style>
         <div className="triangle1 rotate-[-125deg] transition duration-300 ease-in-out hover:translate-x-[6px]"></div>
         <div className="triangle2 rotate-[121deg] transition duration-300 ease-in-out hover:translate-x-[-6px]"></div>
         <div className="triangle3 transition duration-300 ease-in-out hover:translate-y-[4px]"></div>
+        <div className="z-40 absolute top-7 right-10 text-lg font-semibold cursor-default text-blue-950">HG</div>
       </div>
     )
   }
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -34,11 +34,36 @@ const enhance2=id=>{
 
 // enhance2("hg") 
 
-let blob=document.getElementById("blob")
-// console.log(blob)
-window.onpointermove=e=>{
-    const {clientX,clientY}=e
-    blob.animate({
-        left:`${clientX}px`,
-        top:`${clientY}px`},{duration:1000,fill:"forwards"})
-}
+// let blob=document.getElementById("blob")
+// // console.log(blob)
+// window.onpointermove=e=>{
+//     const {clientX,clientY}=e
+//     blob.animate({
+//         left:`${clientX}px`,
+//         top:`${clientY}px`},{duration:1000,fill:"forwards"})
+// }
+
+
+// let prevscrollY = 0;
+// const handleScroll = () => {
+//   const card = document.getElementsByClassName('card')[0]
+//   console.log(card.style)
+//   let scrollPosition = window.scrollY;
+
+//   if (card) {
+//     console.log(card.style)
+//     if (scrollPosition < prevscrollY) {
+//       card.style.top = parseInt(card.style.top)  + (scrollPosition / 100) ;
+//       console.log( parseInt(card.style.top) + (scrollPosition / 100) )
+//     }
+//     else {
+//       card.style.top = parseInt(card.style.top)  - (scrollPosition / 100);
+//       console.log( parseInt(card.style.top)  + (scrollPosition / 100))
+//     }
+
+//   }
+//   scrollPosition;
+//   prevscrollY = scrollPosition;
+// };
+
+// window.addEventListener('scroll', handleScroll);
